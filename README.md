78a2b53b514117365077824e8c9303779f18bdc1
Thomas van Hees <8236465+thomasvanhees@users.noreply.github.com>
2022-06-05 14:06:08 +0200

    add final flake8 plugins

--- README.md
+++ README.md
@@ -26,6 +26,24 @@ Commands:
 - `pycln`: https://hadialqattan.github.io/pycln/#/
 - `flake8`: https://flake8.pycqa.org/en/latest/index.html#quickstart
 
+### `flake8`
+
+Extensions:
+
+- `flake8-annotations`: https://pypi.org/project/flake8-annotations/
+- `flake8-broken-line`: https://pypi.org/project/flake8-broken-line/
+- `flake8-bugbear`: https://pypi.org/project/flake8-bugbear/
+- `flake8-comprehensions`: Helps you write proper comprehensions. See https://pypi.org/project/flake8-comprehensions/.
+- `flake8-eradicate`: Helps find commented out (dead) code. See https://pypi.org/project/flake8-eradicate/.
+- `flake8-quotes`: Something with quotes. See https://github.com/zheller/flake8-quotes
+- `flake8-simplify`: Finds locations where code can be simplified. See https://pypi.org/project/flake8-simplify/.
+- `flake8-tidy-imports`: Simplifies imports. Can also ban imports. See https://pypi.org/project/flake8-tidy-imports/.
+- `flake8-type-checking`: (NOT IMPLEMENTED YET) Moves imports that are only used for type checking into separate blocks. See https://github.com/snok/flake8-type-checking.
+- `flake8-use-fstring`: Forces you to use f-strings. See https://github.com/MichaelKim0407/flake8-use-fstring.
+- `pep8-naming`: Stick to PEP8 standards for names. See https://github.com/PyCQA/pep8-naming.
+- `pep8-docstrings`: Check the format and content of docstrings. See https://github.com/PyCQA/flake8-docstrings and https://www.pydocstyle.org/en/stable/index.html.
+
+
 ### `pytest`
 
 Use `pytest` for writing tests.
--- src/mpspkg/foo.py
+++ src/mpspkg/foo.py
@@ -1,3 +1,8 @@
+"""Foo module for testing purposes.
+
+Adds a couple of functions and classes for testing purposes.
+"""
+
 import logging
 
 import pandas
@@ -7,7 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 def fact(n: int) -> int:
-    """Simple algorithm to calculate the n-th factorial."""
+    """Calculate the n-th factorial.
+
+    :param n: The input variable that determines which factorial is calculated.
+    :returns: The n-th factorial.
+    """
     logger.debug(f"Calculating factorial of {n}.")
     if n == 0:
         return 1
@@ -16,12 +25,42 @@ def fact(n: int) -> int:
 
 
 def uncovered_function(x: str) -> None:
-    """Function not covered by a unit test."""
+    """Form a nice string.
+
+    Function not covered by a unit test.
+
+    :param x: The string you want to modify.
+    :returns: Another string.
+    """
     x = "You entered: " + x
     return x
 
 
 def create_df() -> pandas.DataFrame:
+    """Create a testing Pandas data frame.
+
+    :returns: A data frame.
+    """
     return pandas.DateFrame(
         {"Id": [1, 2, 3], "Name": ["Jaap", "Pim", "Mies"], "Age": [21, 22, 23]}
     )
+
+
+class Dummy:
+    """Dummy class for verifying linting and other checks."""
+
+    bla: str = "hoi"
+
+    def __init__(self, y: str) -> None:
+        """Set the bla parameter.
+
+        :param y: The string to assign to bla.
+        """
+        self.bla = y
+
+    def m(self) -> None:
+        """Return the bla parameter.
+
+        :returns: The bla.
+        """
+        return self.bla
